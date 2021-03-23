3f454e377663b90eea9d409e35c5a9cfdb9fc415
Karol Marszał <mkarolm3005@gmail.com>
2021-03-23 17:18:53 +0100

    Proste cwiczenie z instukcji IF oraz petli

--- LiczbyPierwsze/LiczbyPierwsze.cpp
+++ LiczbyPierwsze/LiczbyPierwsze.cpp
@@ -3,5 +3,17 @@ using namespace std;
 
 int main()
 {
-    
+    int a;
+    cout << "Podaj liczbe a sprawdze czy jest liczba pierwsza: ";
+    cin >> a;       // wczytywanie liczby do sprawdzenia
+    int ile = 0;
+    if (a >= 2)
+    {
+        for (int i = 2; i < a; i++)
+        {
+            if (a % i == 0) ile++;
+        }
+        if (ile > 0) cout << "NIE";
+        else cout << "TAK";
+    }
 }
